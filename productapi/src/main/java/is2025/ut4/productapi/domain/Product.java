c8430ffb198b78d7b2ab985bc1c936eb023dbda1
Matias Alderete <alderetegmatias@gmail.com>
2025-11-06 21:21:58 -0300

    feature:product añadiendo tax y finalPrice como atributos

--- productapi/src/main/java/is2025/ut4/productapi/domain/Product.java
+++ productapi/src/main/java/is2025/ut4/productapi/domain/Product.java
@@ -6,6 +6,9 @@ public class Product {
     private double price;
     private int stock;
 
+    private double tax;
+    private double finalPrice;
+
     public Product() {}
 
     public Product(String code, String description, double price, int stock) {
@@ -13,8 +16,8 @@ public class Product {
         this.description = description;
         this.price = price;
         this.stock = stock;
+        recalcularFinalPrice();
     }
-
     public String getCode() { return code; }
     public void setCode(String code) { this.code = code; }
 
@@ -26,4 +29,12 @@ public class Product {
 
     public int getStock() { return stock; }
     public void setStock(int stock) { this.stock = stock; }
+
+    public double getTax() { return tax; }
+    public void setTax(double tax) {
+        this.tax = tax;
+        recalcularFinalPrice();
+    }
+    public double getFinalPrice() { return finalPrice; } // solo lectura
+    private void recalcularFinalPrice() {this.finalPrice = this.price * this.tax;}
 }
